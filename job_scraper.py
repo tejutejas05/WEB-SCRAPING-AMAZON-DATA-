2875192b1641d3977aed7ce1eaad7977441034f6
TEJAS R <tejasmr05@gmail.com>
2026-03-04 18:15:32 +0530

    json format

--- job_scraper.py
+++ job_scraper.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+
 
 url = "https://realpython.github.io/fake-jobs/"
 
@@ -11,19 +13,33 @@ jobs = soup.find_all("div",class_="card-content")
 print("total jobs found: ", len(jobs))
 print("=" * 50)
 
+job_list =[]
+
 for job in jobs:
     title = job.find("h2",class_="title").text.strip()
     company = job.find("h3",class_="company").text.strip()
-    loction = job.find("p",class_="location").text.strip()
+    location = job.find("p",class_="location").text.strip()
 
     link_tag = job.find("a",string="Apply")
     link = link_tag["href"] if link_tag else "No Link"
 
-    print("title:", title)
-    print("company:", company)
-    print("Loction:", loction)
-    print("Apply Link: ", link)
-    print("-" * 50)
+    job_data = {
+        "title":title,
+        "company":company,
+        "location":location,
+        "link":link
+    }
+
+    job_list.append(job_data)
+
+    with open("jobs.json" , "w", encoding="utf-8") as f:
+        json.dump(job_list, f, indent=4)
+
+print("saved as jobs.json")
+
+print("Toatal jobs collected:", len(job_list))
+
+
 
 
 
